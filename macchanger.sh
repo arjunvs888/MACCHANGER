bb932b70c2aade97bd84ffab9996f8b2aa1899e1
V S Arjun <32426492+arjunvs888@users.noreply.github.com>
2020-04-30 05:59:35 +0000

    Add files via upload

--- macchanger.sh
+++ macchanger.sh
@@ -5,19 +5,32 @@ then
 	printf "No Network Interfaces found!!!!\n Check the Network and try again\n"
 	exit
 else
-	echo "Network Interfaces running ==> $nic"
-	for i in $nic; do
-		ifconfig $i down
-		echo "$i is down....."
-		sleep 1
-		macchanger -r $i
-		sleep 1
-		ifconfig $i up
-		echo "$i is up....."
-		sleep 1
+	printf "Network Interfaces running\t ==> $nic\n"
+	printf "Only one interface should run at a time! Disable the others.\n "
+	while true
+	do
+	read -p "Run the script now?(Y/N) " answer
+	  case $answer in
+	   [yY]* ) for i in $nic; do
+			ifconfig $i down
+			echo "$i is down....."
+			sleep 1
+			macchanger -r $i
+			sleep 1
+			ifconfig $i up
+			echo "$i is up....."
+			sleep 1
+		   done
+			echo "....................."
+			sleep 2
+			echo "Setting DNS....."
+			source = /root/scripts/dns_set.sh
+			./dns_set.sh
+			echo "OPERATION SUCCESSFUL"
+		   break;;
+	   [nN]* ) exit;;
+	   * )     echo "Just enter Y or N, please.";;
+	  esac
 	done
-	echo "....................."
-	sleep 2
-	echo "OPERATION SUCCESSFUL"
 fi
 
